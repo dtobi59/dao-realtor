dc9dfb7b3df88f3adc235965e13c9b6118ba4e45
Avrahm <avrahmk@me.com>
2022-03-05 23:18:49 -0500

    tweak design project submission

--- client/components/ProjectSubmissionForm.jsx
+++ client/components/ProjectSubmissionForm.jsx
@@ -107,84 +107,82 @@ export default function ProjectSubmissionForm({ setLoading }) {
     }
 
     return (
-        <div className="container">
-            <div className="flex flex-wrap md:justify-between">
-                <div className="w-full md:w-1/2 lg:w-6/12 px-4  place-content-center">
-                    <div className="rounded-lg p-2  shadow-lg  text-center">
-                        <h2 className="text-2xl font-bold">Project Name</h2>
-                        <input type="text" className="bg-gray-200 w-full px-2 py-1 rounded text-black" disabled={files.length > 0 ? true : false}
-                            placeholder="Project Name" value={name || ''} onChange={(e) => setName(e.target.value)} />
-                        <div className="py-4  text-center">
-                            <div className="mb-6">
-                                <p className="text-center">Description</p>
-                                <textarea type="text" className="bg-gray-200 w-full px-2 py-1 rounded text-black"
-                                    placeholder="Description" value={description.description || ''} onChange={(e) => setDescription({ ...description, description: e.target.value })} />
-                            </div>
-                            <div className="mb-6">
-                                <p className="text-center">Price</p>
+        <div className="flex flex-wrap md:justify-between pb-10">
+            <div className="w-full md:w-1/2 lg:w-6/12 px-4  place-content-center">
+                <div className="rounded-lg p-2  shadow-lg  text-center">
+                    <h2 className="text-2xl font-bold">Project Name</h2>
+                    <input type="text" className="bg-gray-200 w-full px-2 py-1 rounded text-black" disabled={files.length > 0 ? true : false}
+                        placeholder="Project Name" value={name || ''} onChange={(e) => setName(e.target.value)} />
+                    <div className="py-4  text-center">
+                        <div className="mb-6">
+                            <p className="text-center">Description</p>
+                            <textarea type="text" className="bg-gray-200 w-full px-2 py-1 rounded text-black"
+                                placeholder="Description" value={description.description || ''} onChange={(e) => setDescription({ ...description, description: e.target.value })} />
+                        </div>
+                        <div className="mb-6">
+                            <p className="text-center">Price</p>
+                            <input type="number" className="bg-gray-200 w-full px-2 py-1 rounded text-black"
+                                placeholder="Price" value={price || ''} onChange={(e) => setPrice(e.target.value)} />
+                        </div>
+                        <div className="mb-6 flex flex-row justify-center gap-4">
+                            <div className="mb-2">
+                                <p className="text-center">Bedrooms</p>
                                 <input type="number" className="bg-gray-200 w-full px-2 py-1 rounded text-black"
-                                    placeholder="Price" value={price || ''} onChange={(e) => setPrice(e.target.value)} />
+                                    placeholder="5" value={description.bedrooms || ''} onChange={(e) => setDescription({ ...description, bedrooms: e.target.value })} />
                             </div>
-                            <div className="mb-6 flex flex-row justify-center gap-4">
-                                <div className="mb-2">
-                                    <p className="text-center">Bedrooms</p>
-                                    <input type="number" className="bg-gray-200 w-full px-2 py-1 rounded text-black"
-                                        placeholder="5" value={description.bedrooms || ''} onChange={(e) => setDescription({ ...description, bedrooms: e.target.value })} />
-                                </div>
-                                <div className="mb-2">
-                                    <p className="text-center">Bathrooms</p>
-                                    <input type="number" className="bg-gray-200 w-full px-2 py-1 rounded text-black"
-                                        placeholder="3.5" value={description.bathrooms || ''} onChange={(e) => setDescription({ ...description, bathrooms: e.target.value })} />
-                                </div>
-                                <div className="mb-2">
-                                    <p className="text-center">Area (sq ft)</p>
-                                    <input type="number" className="bg-gray-200 w-full px-2 py-1 rounded text-black"
-                                        placeholder="2000" value={description.area || ''} onChange={(e) => setDescription({ ...description, area: e.target.value })} />
-                                </div>
+                            <div className="mb-2">
+                                <p className="text-center">Bathrooms</p>
+                                <input type="number" className="bg-gray-200 w-full px-2 py-1 rounded text-black"
+                                    placeholder="3.5" value={description.bathrooms || ''} onChange={(e) => setDescription({ ...description, bathrooms: e.target.value })} />
                             </div>
-                            <div className="mb-6 flex flex-row justify-center gap-4">
-                                <div className="mb-4">
-                                    <p className="text-center">Address</p>
-                                    <input type="text" className="bg-gray-200 w-full px-2 py-1 rounded text-black"
-                                        placeholder="Address" value={description.address || ''} onChange={(e) => setDescription({ ...description, address: e.target.value })} />
-                                </div>
-                                <div className="mb-2">
-                                    <p className="text-center">Country</p>
-                                    <input type="text" className="bg-gray-200 w-full px-2 py-1 rounded text-black"
-                                        placeholder="Country" value={description.country || ''} onChange={(e) => setDescription({ ...description, country: e.target.value })} />
-                                </div>
+                            <div className="mb-2">
+                                <p className="text-center">Area (sq ft)</p>
+                                <input type="number" className="bg-gray-200 w-full px-2 py-1 rounded text-black"
+                                    placeholder="2000" value={description.area || ''} onChange={(e) => setDescription({ ...description, area: e.target.value })} />
                             </div>
-                            <div className="mb-6 flex flex-row justify-center gap-4">
-                                <div className="mb-2">
-                                    <p className="text-center">Longitude</p>
-                                    <input type="text" className="bg-gray-200 w-full px-2 py-1 rounded text-black"
-                                        placeholder="Latitude" value={latitude || ''} onChange={(e) => setLatitude(e.target.value)} />
-                                </div>
-                                <div className="mb-2">
-                                    <p className="text-center">Latitude</p>
-                                    <input type="text" className="bg-gray-200 w-full px-2 py-1 rounded text-black"
-                                        placeholder="Longitude" value={longitude || ''} onChange={(e) => setLongitude(e.target.value)} />
-                                </div>
+                        </div>
+                        <div className="mb-6 flex flex-row justify-center gap-4">
+                            <div className="mb-4">
+                                <p className="text-center">Address</p>
+                                <input type="text" className="bg-gray-200 w-full px-2 py-1 rounded text-black"
+                                    placeholder="Address" value={description.address || ''} onChange={(e) => setDescription({ ...description, address: e.target.value })} />
+                            </div>
+                            <div className="mb-2">
+                                <p className="text-center">Country</p>
+                                <input type="text" className="bg-gray-200 w-full px-2 py-1 rounded text-black"
+                                    placeholder="Country" value={description.country || ''} onChange={(e) => setDescription({ ...description, country: e.target.value })} />
+                            </div>
+                        </div>
+                        <div className="mb-6 flex flex-row justify-center gap-4">
+                            <div className="mb-2">
+                                <p className="text-center">Longitude</p>
+                                <input type="text" className="bg-gray-200 w-full px-2 py-1 rounded text-black"
+                                    placeholder="Latitude" value={latitude || ''} onChange={(e) => setLatitude(e.target.value)} />
+                            </div>
+                            <div className="mb-2">
+                                <p className="text-center">Latitude</p>
+                                <input type="text" className="bg-gray-200 w-full px-2 py-1 rounded text-black"
+                                    placeholder="Longitude" value={longitude || ''} onChange={(e) => setLongitude(e.target.value)} />
                             </div>
                         </div>
                     </div>
                 </div>
-                <div className="w-full md:w-1/2 lg:w-6/12 px-4 sm:flex sm:justify-center">
-                    <div className="max-w-[570px] pt-2 ">
+            </div>
+            <div className="w-full md:w-1/2 lg:w-6/12 px-4 sm:flex sm:justify-center sm:mb-20">
+                <div className="max-w-[570px] pt-2 ">
 
-                        <div className="pb-2  text-center">
-                            <p className="text-center pb-3">Upload Images</p>
-                            <div className="flex flex-wrap justify-around gap-4">
-                                {files && files.map((file, index) => { return <Image src={file.preview} width="70" height="70" onClick={clearImage} key={index} /> })}
-                            </div>
+                    <div className="pb-2  text-center">
+                        <p className="text-center pb-3">Upload Images</p>
+                        <div className="flex flex-wrap justify-around gap-4">
+                            {files && files.map((file, index) => { return <Image src={file.preview} width="150" height="150" onClick={() => clearImage(index)} key={index} /> })}
                         </div>
-                        <hr />
-                        <div className="">
-                            <input type="file" accept="image/jpg,image/png" name="image" className="py-3" id="image" ref={fileInputRef} disabled={!name || files.length >= 5 && true} onChange={handleFileChange} />
-                            <button onClick={handleSubmit} className="bg-[#2952e3] block rounded cursor-pointer px-2 py-1 hover:bg-[#2546bd]">
-                                Submit
-                            </button>
-                        </div>
+                    </div>
+                    <hr />
+                    <div className="pb-20">
+                        <input type="file" accept="image/jpg,image/png" name="image" className="py-3" id="image" ref={fileInputRef} disabled={!name || files.length >= 5 && true} onChange={handleFileChange} />
+                        <button onClick={handleSubmit} className="bg-[#2952e3] block rounded cursor-pointer px-2 py-1 hover:bg-[#2546bd]">
+                            Submit
+                        </button>
                     </div>
                 </div>
             </div>
