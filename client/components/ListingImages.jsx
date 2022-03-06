df04f2cc034a0344b3aabda5a7ddf36adc4270be
Avrahm <avrahmk@me.com>
2022-03-06 13:21:17 -0500

    updated listingImages with getLinks web3storage

--- client/components/ListingImages.jsx
+++ client/components/ListingImages.jsx
@@ -1,43 +1,55 @@
 import Image from "next/image";
+import { useEffect, useState } from "react";
+import { getLinks } from "../lib/web3storage";
 
-function importAll(r) {
-    let images = {};
-    r.keys().map((item, index) => {
-        return (images[item.replace("./", "")] = r(item));
+const Images = ({ data }) => {
+    if (!data) return null;
+    return data.map((item, index) => {
+        return (
+            <div className="sm:max-w-[250px] sm:h-[150px] md:max-w-50px md:float-right" key={index}>
+                <Image objectFit="cover" src={`/api/imageProxy?imageUrl=${item.src}`} alt={item.alt || ""} width={1000} height={600} />
+            </div>
+        );
     });
-    return images;
-}
-
-const images = importAll(
-    require.context("../assets/images", false, /\.(png|jpe?g|svg|gif)$/)
-);
+};
 
 export default function ListingImages({ id, data }) {
-    const frontImg = `home_front_${id}.jpeg`;
-    const interiorImg = `home_interior_${id}.jpeg`;
-    const bathImg = `home_bath_${id}.jpeg`;
-    const floorplanImg = `home_floorplan_${id}.jpeg`;
-    const floorplanAImg = `home_floorplan_${id}a.jpeg`;
+
+    const [loading, setLoading] = useState(true);
+    const [images, setImages] = useState([]);
+
+    const getImages = async () => {
+        const cid = data.image_hash
+        const links = await getLinks(cid);
+        const imageData = [];
+        if (!links) return imageData;
+        const url = `https://${cid}.ipfs.dweb.link/`
+        links.forEach((link) => {
+            imageData.push({
+                src: url + link.name,
+                alt: link.name
+            });
+        });
+        setImages(imageData);
+        setLoading(false);
+    };
+
+    useEffect(() => {
+        getImages();
+    }, []);
+
+    console.log(images);
+
 
     return (
         <div className="flex flex-col justify-center gap-4 px-3 py-5 md:flex-row">
-            <div className="md:max-w-[550px] w-full">
-                <Image objectFit="cover" src={images[frontImg]} alt={data.title} />
-            </div>
-            <div className="grid grid-cols-4 gap-4 place-content-center md:grid-cols-2">
-                <div className="md:max-w-[250px] h-[150px]">
-                    <Image objectFit="cover" src={images[interiorImg]} alt={data.title} />
-                </div>
-                <div className="max-w-[250px]">
-                    <Image objectFit="cover" src={images[bathImg]} alt={data.title} />
-                </div>
-                <div className="max-w-[250px]">
-                    <Image objectFit="cover" src={images[floorplanImg]} alt={data.title} />
-                </div>
-                <div className="max-w-[250px]">
-                    <Image objectFit="cover" src={images[floorplanAImg]} alt={data.title} />
-                </div>
-            </div>
+            {loading ? (
+                <>Loading Images...</>
+            ) : (
+                <>
+                    <Images data={images} />
+                </>
+            )}
         </div>
     )
 }
